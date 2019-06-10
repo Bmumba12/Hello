6e1c8677b244464041af7f2c4e8fe40d7ffc0234
Bmumba12 <brianmumba0@gmail.com>
2019-06-10 14:24:20 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+#name1 = input('What is your name?   ')
+#ycolor = input('what is your favorite?  ')
+#print(name1 + ' likes ' + ycolor)
+
+#birth_year = input('Birth Year:  ')
+#age = 2019 - int(birth_year)
+#print(age)
+
+# "Another function to convert respondents weight in pound to kg"
+#weight_lb = input('What is your weight in pound? ')
+#weigh_kg = int(weight_lb) * 0.454
+#print(weigh_kg)
+
+#Strings
+
+emal = '''
+Hello Mumba,
+
+I have been waiting to hear from you today.
+Please return as soon as you receive this message.
+
+Thanks
+
+'''
+print(emal)
