5e28695078f70a1f583c7a735008955b67c4f161
Bmumba12 <brianmumba0@gmail.com>
2019-06-10 21:43:08 +0300

    This is my first commit

---
+++ Weight converter.py
@@ -0,0 +1,10 @@
+# This creates a weight converter program to convert users weight to pounds or kilos.
+
+weight= int(input('weight: '))
+unit= input('(L)bs or (K)g: ')
+if unit.upper()== "L":
+    converted=weight*0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight/0.45
+    print(f"You are {converted} pound")
