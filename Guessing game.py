e642da7eb543afb35e548beda20a930cf3589ca4
Bmumba12 <brianmumba0@gmail.com>
2019-06-10 23:07:10 +0300

    This is my second commit

--- Guessing game.py
+++ Guessing game.py
@@ -11,3 +11,30 @@ while guess_count < guess_limit:
         break
 else:
     print("Sorry you have failed the game")
+
+# The car game
+command = ""
+started = False
+while True:
+    command =input(">").lower()
+    if command=="start":
+        if started:
+            print("Car already started")
+        else:
+            started = True
+            print("Car started")
+    elif command=="stop":
+        if not started:
+            print("Car already stopped")
+        else:
+            started=False
+            print("Car stopped")
+    elif command == "help":
+        print("""
+Start = to start the car
+Stop = to stop the car
+quit = to quit """)
+    elif command=="quit":
+        break
+else:
+    print("Sorry I don't understand")
